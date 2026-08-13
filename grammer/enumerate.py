b7ad91c656bc4a7293426259445d0c84f43cede8
dlcodns <redmond0412@gmail.com>
2026-08-13 14:00:23 +0900

    start 파라미터

--- grammer/enumerate.py
+++ grammer/enumerate.py
@@ -3,3 +3,7 @@ fruits = ["사과", "바나나", "체리"]
 
 for i, fruit in enumerate(fruits):
     print(f'{i}. {fruit} ')
+    
+# start 파라미터 - 인덱스를 1부터 시작하기
+for i, fruit in enumerate(fruits, start=1):
+    print(f'{i}번째 과일: {fruit}')
