44df9c279a753f876cf52457b09501bf8212533d
dlcodns <redmond0412@gmail.com>
2026-08-13 13:47:47 +0900

    List Comprehension

--- grammer/filter.py
+++ grammer/filter.py
@@ -1,7 +1,10 @@
 import sys
 
 nums = [3,5,4,8,9]
-nums = list(filter(lambda n: n%2 != 0, nums))
+nums1 = list(filter(lambda n: n%2 != 0, nums))
+nums2 = [n for n in nums if n%2 == 0]
 
-print(nums)
-sys.stdout.write(str(nums))
+print(nums1)
+sys.stdout.write(str(nums1)+"\n")
+print(nums2)
+sys.stdout.write(str(nums2)+"\n")
