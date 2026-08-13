4c0d9ffd55da0960575d97044f54d20f15856498
dlcodns <redmond0412@gmail.com>
2026-08-13 13:38:24 +0900

    빠른 입출력 코드

--- grammer/stdin.py
+++ grammer/stdin.py
@@ -5,3 +5,11 @@ N = int(sys.stdin.readline().rstrip())
 
 sys.stdout.write(S+"\n")
 sys.stdout.write(str(N)+"\n")
+
+input = sys.stdin.readline
+print = sys.stdout.write
+S = input().rstrip()
+N = int(input().rstrip())
+
+print(S+"\n")
+print(str(N)+"\n")
